762279e6f9d61c833e0113c0c254dbbe27462168
shiba <hdswick@gmail.com>
2019-02-04 19:11:50 -0600

    Started basic go server

---
+++ server.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log"
+	"fmt"
+	"net/http"
+	"encoding/json"
+)
+
+type Task struct {
+	Name string `json:"name"`
+	Status string `json:"status"`
+}
+
+type Tasks []Task
+
+func tasksHandler(w http.ResponseWriter, r *http.Request) {
+	tasks := Tasks{
+		Task{Name: "doing stuff", Status: "Todo"},
+		Task{Name: "doing things", Status: "Done"},
+		Task{Name: "doing thing", Status: "OnGoing"},
+	}
+
+	json.NewEncoder(w).Encode(tasks)
+}
+
+func routes() {
+	http.Handle("/", http.FileServer(http.Dir("./static")))
+	http.HandleFunc("/tasks", tasksHandler)
+}
+
+func main() {
+	routes()
+	fmt.Println("Running Kanelm server at port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
