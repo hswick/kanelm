e32de05e97623b0729bf34325be46de22ed8bca4
shiba <hdswick@gmail.com>
2019-02-07 12:25:16 -0600

    Mocked out new routes for server

--- server.go
+++ server.go
@@ -24,9 +24,33 @@ func tasksHandler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(tasks)
 }
 
+func deleteTaskHandler(w http.ResponseWriter, r *http.Request) {
+	tasks := Tasks{
+		Task{Name: "doing stuff", Status: "Todo"},
+		Task{Name: "doing things", Status: "Done"},
+		Task{Name: "doing thing", Status: "OnGoing"},
+	}
+
+	json.NewEncoder(w).Encode(tasks)
+	
+}
+
+func moveTaskHandler(w http.ResponseWriter, r *http.Request) {
+	tasks := Tasks{
+		Task{Name: "doing stuff", Status: "Todo"},
+		Task{Name: "doing things", Status: "Done"},
+		Task{Name: "doing thing", Status: "OnGoing"},
+	}
+
+	json.NewEncoder(w).Encode(tasks)
+	
+}
+
 func routes() {
 	http.Handle("/", http.FileServer(http.Dir("./static")))
 	http.HandleFunc("/tasks", tasksHandler)
+	http.handleFunc("/delete", deleteTaskHandler)
+	http.handleFunc("/move", moveTaskHandler)
 }
 
 func main() {
