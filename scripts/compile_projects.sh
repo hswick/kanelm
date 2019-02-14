d66300fbc844eb7a1aeb72f89e560516322c05ae
shiba <hdswick@gmail.com>
2019-02-13 19:16:11 -0600

    Moved source code around, split up compile scripts

--- scripts/compile_projects.sh
+++ scripts/compile_projects.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-js="elm.js"
-min="elm.min.js"
+js="projects.js"
+min="projects.min.js"
 
-elm make --optimize --output=$js $@
+elm make --optimize --output=$js src/projects/Main.elm
 
 uglifyjs $js --compress 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9",pure_getters,keep_fargs=false,unsafe_comps,unsafe' | uglifyjs --mangle --output=$min
 
@@ -13,5 +13,5 @@ echo "Compiled size:$(cat $js | wc -c) bytes  ($js)"
 echo "Minified size:$(cat $min | wc -c) bytes  ($min)"
 echo "Gzipped size: $(cat $min | gzip -c | wc -c) bytes"
 
-rm -f elm.js
-mv elm.min.js static
+rm -f $js
+mv $min static
